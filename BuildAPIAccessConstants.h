1dc31de1a99c13c58a6dbfe23962015182ecea11
Tony Smith <tony@electricimp.com>
2015-07-10 09:06:52 +0100

    Create BuildAPIAccessConstants.h

---
+++ BuildAPIAccessConstants.h
@@ -0,0 +1,39 @@
+
+//  Created by Tony Smith on 30/06/2015.
+//  Copyright (c) 2015 Tony Smith. All rights reserved.
+//  Issued under the MIT licence
+
+
+#ifndef Squinter_BuildAPIAccessContants_h
+#define Squinter_BuildAPIAccessContants_h
+
+
+#define kConnectTypeNone					0
+#define kConnectTypeGetModels				1
+#define kConnectTypeGetDevices				2
+#define kConnectTypePostCode				3
+#define kConnectTypeNewModel                4
+#define kConnectTypeRestartDevice           5
+#define kConnectTypeAssignDeviceToModel     6
+#define kConnectTypeDeleteModel             7
+#define kConnectTypeDeleteDevice            8
+#define kConnectTypeUpdateDevice            9
+#define kConnectTypeUpdateModel             10
+#define kConnectTypeGetCodeLatestBuild      11
+#define kConnectTypeGetCodeRev              12
+#define kConnectTypeGetLogEntries           13
+#define kConnectTypeGetLogEntriesRanged		14
+#define kConnectTypeGetLogEntriesStreamed   15
+
+#define kConnectTypeGetRevisions			99
+#define kConnectTypeGetRevision				99
+#define kConnectTypeLogin					100
+
+#define kBaseAPIURL @"https://build.electricimp.com"
+#define kAPIVersion @"/v4/"
+
+#define kAllLogs 0
+#define kStreamLogs -1
+
+
+#endif
